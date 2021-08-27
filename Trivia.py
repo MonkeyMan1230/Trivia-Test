1805a5fb9259ac8847f6564941b3605b2e9290a1
MonkeyMan1230 <88180835+MonkeyMan1230@users.noreply.github.com>
2021-08-26 22:46:26 -0400

    Update Trivia.py

--- Trivia.py
+++ Trivia.py
@@ -1,13 +1,12 @@
-## 10 question game made in python.
-## Please use all lower case for your answers.
 print('Hello, welcome to the Trivia Quiz!')
 print('Try and answer them the best you can') 
-print('Are you ready?(Yes/No): ')
-	
-TotalQuestions = 5
-Score = 0
 
-if ans.lower() 'yes':
+print('Are you ready? (Yes/No): ')
+	
+total_q = 5
+score = 0
+
+if ans.lower() == 'Yes':
    ans = input('1. Is my favorite color red? ')
     if ans.lower() == 'yes' :
         Score += 1
