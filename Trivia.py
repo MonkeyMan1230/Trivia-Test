43fe8bc760f1fd5677c837783abda8cac2ea74b6
MonkeyMan1230 <88180835+MonkeyMan1230@users.noreply.github.com>
2021-08-27 20:15:24 -0400

    Update Trivia.py

--- Trivia.py
+++ Trivia.py
@@ -1,15 +1,1 @@
-print('Hello, welcome to the Trivia Quiz!')
-print('Try and answer them the best you can') 
-
-print('Are you ready? (Yes/No): ')
-	
-total_q = 5
-score = 0
-
-if ans.lower() == 'Yes':
-   ans = input('1. Is my favorite color red? ')
-    if ans.lower() == 'yes' :
-        Score += 1
-        print('Correct')
-    else:
-        print('Incorrect')
+https://www.anyfiddle.com/p/monkeyman1/9e6ca7o3
