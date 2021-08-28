c998639cc336b1d5c1e4e09b2adff39c34eb419b
MonkeyMan1230 <88180835+MonkeyMan1230@users.noreply.github.com>
2021-08-27 20:56:43 -0400

    Update Trivia.py

--- Trivia.py
+++ Trivia.py
@@ -1,1 +1,47 @@
-https://www.anyfiddle.com/p/monkeyman1/9e6ca7o3
+print('Hello, Welcome to Brians Trivia Quiz!')
+print('Try your best at answering!')
+
+totalquestions = 3
+score = 0
+
+ans = input('Are you ready Yes/No?')
+
+if ans.lower() == 'yes':
+
+    ans = input('1. Is my favorite color red?')
+
+    if ans.lower() == 'yes':
+      print('Correct!')
+      score += 1
+    else:
+        print('Incorrect')
+
+    ans = input('2. What is 8 x 8 + 4=')
+
+    if ans.lower() == '68':
+      print('Correct!')
+      score += 1
+    else:
+        print('Incorrect')
+
+    ans = input('3. Say yes for free points!!!')
+
+    if ans.lower() == 'yes':
+      print('Correct!')
+      score += 1
+    else:
+        print('Incorrect')
+
+print('You got', score, "Questions Correct.")
+mark = (score/totalquestions) * 100
+
+print("Mark:", mark)
+print('Thanks For Playing, Good Bye!!!!')
+
+<!doctype html>
+ <head>
+  <body>
+<h1>#Made by The Special#3116</h1>
+   <pm>#Thanks For Trying</pm>
+  </body>
+ </head>
